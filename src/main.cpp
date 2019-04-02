d8ed08473fdeae188973c6781894a45f5018519a
Ishan Ranade <ishanranade@Ishans-MacBook-Pro.local>
2019-04-02 08:09:43 -0700

    add imgui and glm libraries

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
 
 int main() {
     std::cout << "hello" << std::endl;
+    glm::vec3 x = glm::vec3(0.0f);
+    std::cout << x.x << std::endl;
     return 0;
 }
